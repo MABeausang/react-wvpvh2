ee0b51d3808d879c060fcc60f907143eb0bac46f
MABeausang <90843438+MABeausang@users.noreply.github.com>
2023-03-14 16:45:25 +0100

    4 files modified, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React, { useEffect, useState } from 'react';
-
 /* import React from 'react'; */
+import React, { useEffect, useState } from 'react';
 import './style.css';
 import Header from './header.js'; // importerar header
-import Film from './film.js';
 
 export default function App() {
-  // Usestate variabler.
+  // Usestate variabler
   const [films, setFilms] = useState([]);
   const [title, setTitle] = useState('');
   const [director, setDirector] = useState('');
@@ -16,25 +14,19 @@ export default function App() {
   const [inputText, setInputText] = useState('');
   const [counter, setCounter] = useState(0);
 
+  // Hämtar information från API och lagrar i variabeln Films
   useEffect(() => {
     fetch('https://swapi.dev/api/films/')
       .then((response) => response.json())
       .then((data) => setFilms(data.results)); // Innehåller alla JSON data
   }, []);
 
-  function showTitle() {
-    setTitle(films[0].title);
-    setDirector(films[0].director);
-    setProducer(films[0].producer);
-    setOpening_crawl(films[0].opening_crawl);
-    setRelease_date(films[0].release_date);
-  }
-
+  // Funktionen lagrar värdet från sökfältet i variabeln InputText
   const changeInputText = (e) => {
     setInputText(e.target.value);
-    //...
   };
 
+  // Funktionen läser av knapp-trycking. Vid Enter påbörjar vi vår sökning genom funktionen Match()
   function handleSearch(event) {
     if (event.key === 'Enter') {
       console.log('Jag har sökt på: ' + event.target.value);
@@ -42,11 +34,12 @@ export default function App() {
     }
   }
 
+  // Funktionen matachar sökordet mot titel, director och opening crawl.
+  // Använder sig av en for-loop för att gå igenom films-variabeln som är en array av filmerna.
   function Match() {
     for (const film of films) {
-      /*  console.log(film.title);
-      console.log(inputText); */
       if (
+        // if-sats med OR  som avgör om titel, director eller opening crawl matchar mot sökordet.
         film.title.toLowerCase().includes(inputText.toLowerCase()) ||
         film.director.toLowerCase().includes(inputText.toLowerCase()) ||
         compareStrings(film.opening_crawl, inputText)
@@ -63,6 +56,8 @@ export default function App() {
     Updatecounter();
   }
 
+  // Funktion med for-loop som går igenom om sökordet finns i filmtiteln, director eller opening crawl.
+  // Om det blir en matchning retuneras en siffra med antalet matchningar.
   function Updatecounter() {
     let i = 0;
     for (const film of films) {
@@ -77,6 +72,7 @@ export default function App() {
     setCounter(i);
   }
 
+  // Funktion som jämför två strängar med text och returnerar true om sträng b finns i sträng a. I vår kod används detta för opening crawl. Behövs egentligen inte, utan functionen skrevs för att testa ett alternativ till att korta ner OR-satserna i match() ovan.
   function compareStrings(a, b) {
     let match = false;
     if (a.toLowerCase().includes(b.toLowerCase())) match = true;
@@ -86,7 +82,7 @@ export default function App() {
   return (
     <div>
       <Header />
-      <input
+      <input /* sökruta för vårt sökord, har funktioner bundna om användaren skriver i fältet (förklarade ovan. ) */
         type="text"
         placeholder="Search"
         className="searchBox"
@@ -95,16 +91,16 @@ export default function App() {
         value={inputText}
         disabled={films.length === 0}
       ></input>
-      <p className="resultcounter">Antalet filmer som matchar: {counter}</p>
-      <section className="container">
-        <div>Titel: {title}</div>
-        <div>Release date: {release_date}</div>
-        <div>Director: {director}</div>
-        <div>Producer: {producer}</div>
-        <div>Opening crawl: {opening_crawl}</div>
+      <section className="movieresult">
+        {' '}
+        {/* resultatbox för  vår hittade film. */}
+        <ul className="movietext">Titel: {title}</ul>
+        <ul className="movietext">Release date: {release_date}</ul>
+        <ul className="movietext">Director: {director}</ul>
+        <ul className="movietext">Producer: {producer}</ul>
+        <ul className="movietext">Opening crawl: {opening_crawl}</ul>
       </section>
+      <p className="resultcounter">Antalet filmer som matchar: {counter}</p>
     </div>
   );
 }
-
-//<Film filmtitel={title} /> {/* prop som har värdet title  */}
--- src/header.js
+++ src/header.js
@@ -3,8 +3,8 @@ import React, { useEffect, useState } from 'react';
 export default function Header() {
   return (
     <div>
-      <h1>STAR WARS! DU DU… DUDUDU DU… DUDUDU DU… DUDUDUDU</h1>
-      <p>A long time ago in a galaxy far, far away...</p>
+      <h1>STAR WARS!</h1>
+      <h2>A long time ago in a galaxy far, far away...</h2>
     </div>
   );
 }
